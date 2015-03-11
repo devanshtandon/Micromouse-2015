391ba0dad3ed1329865ab044266166f5ff0809b2
chrisdats <christopher.datsikas@gmail.com>
2015-03-11 17:08:59 -0400

    updated motor driver test code

---
+++ TestCode/TB6612FNG-TEST/TB6612FNG-TEST.ino
@@ -0,0 +1,50 @@
+/* TB6612FNG-TEST.ino
+Christopher Datsikas
+Created: Feb 2015
+Last Modified: 03-11-2015
+Status: Works
+
+Test the Pololu TB6612FNG Dual Motor Driver Carrier
+connected to an Arduino Nano and Pololu Micro Metal Gearmotor
+// http://www.embeddedrelated.com/showarticle/498.php
+ */
+
+// Define pins for Motor Driver TB6612FNG
+#define PWMA 5
+#define PWMB 6
+
+#define AIN2 2
+#define AIN1 3
+#define BIN1 7
+#define BIN2 8
+
+#define STBY 4
+
+void setup() {
+  // enables serial commnication
+  Serial.begin(9600);
+  
+  // set all the Motor Driver pins tp OUTPUT
+  pinMode(PWMA, OUTPUT);
+  pinMode(PWMB, OUTPUT);
+  pinMode(AIN1, OUTPUT);
+  pinMode(AIN2, OUTPUT);
+  pinMode(BIN1, OUTPUT);
+  pinMode(BIN2, OUTPUT);
+  pinMode(STBY, OUTPUT);
+  digitalWrite(STBY, HIGH);  // turns motor driver on
+}
+
+void loop() {
+  // turns motor clockwise
+  digitalWrite(AIN1, HIGH);
+  digitalWrite(AIN2, LOW);
+  analogWrite(PWMA, 100);    // uses PWM to set motor speed
+  delay(5000);            // wait for five seconds
+  
+   // turn motor counter-clockwise
+  digitalWrite(AIN1, LOW); 
+  digitalWrite(AIN2, HIGH);
+  analogWrite(PWMA, 200);   // uses PWM to set motor speed
+  delay(5000);              // wait for five seconds
+}
