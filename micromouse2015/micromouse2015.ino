10c32b365a661d3e0ce7846a55b89c984edac086
Devansh <devanshtandon@gmail.com>
2015-04-07 21:58:03 -0400

    pushed PID

--- micromouse2015/micromouse2015.ino
+++ micromouse2015/micromouse2015.ino
@@ -159,25 +159,7 @@ void loop() {
 // forward one square function -- needs motor constants
 // turn functions -- motor constants
 // backward function -- motor constants
-void forward (double speed, int encoderCount)
-{
-  while((abs(countsLeft+countsRight)/2) < encoderCount) 
-  {
-    // turn on PID
-    Input = sensorValues[1] - sensorValues[3];
-    myPID.SetMode(AUTOMATIC);
-    myPID.Compute();
 
-    analogWrite(PWMA, speed-Output);
-    analogWrite(PWMB, speed+Output);
-
-    countsLeft = encoders.getcountsLeft();
-    countsRight = encoders.getcountsRight();
-  }
-
-  PORTD &= B01110011; // stop
-  PORTB &= 255-(1<<0);
-}
 
 //Give a direction (FORWARD, BACKWARD, LEFT, or RIGHT)
 //and the number of encoder counts to move.
@@ -215,8 +197,15 @@ void go(int direction, int counts) {
   }
 
   while(abs(countsLeft) < counts || abs(countsRight) < counts) {
+    // turn on PID
+    Input = sensorValues[1] - sensorValues[3];
+    myPID.SetMode(AUTOMATIC);
+    myPID.Compute();
+
     countsLeft = encoders.getcountsLeft();
     countsRight = encoders.getcountsRight();
+
+    getSensors();
   }
 
   PORTD &= B01110011; // stop
